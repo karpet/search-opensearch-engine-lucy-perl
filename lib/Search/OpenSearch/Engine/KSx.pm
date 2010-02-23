8fcd37036714392bf13e44bab2d47c5cbbc92d44
Peter Karman <karpet@peknet.com>
2010-02-23 05:26:43 +0000

    facet support; release 0.02

--- lib/Search/OpenSearch/Engine/KSx.pm
+++ lib/Search/OpenSearch/Engine/KSx.pm
@@ -4,14 +4,68 @@ use warnings;
 use Carp;
 use base qw( Search::OpenSearch::Engine );
 use SWISH::Prog::KSx::Searcher;
+use KinoSearch::Object::BitVector;
+use KinoSearch::Search::HitCollector::BitCollector;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub init_searcher {
     my $self     = shift;
     my $index    = $self->index or croak "index not defined";
     my $searcher = SWISH::Prog::KSx::Searcher->new( invindex => $index );
     return $searcher;
+}
+
+sub build_facets {
+    my $self     = shift;
+    my $query    = shift or croak "query required";
+    my $results  = shift or croak "results required";
+    my $facetobj = $self->facets or return;
+
+    my @facet_names  = @{ $facetobj->names };
+    my $sample_size  = $facetobj->sample_size;
+    my $searcher     = $self->searcher;
+    my $ks_searcher  = $searcher->{ks};
+    my $query_parser = $searcher->{qp};
+    my $bit_vec      = KinoSearch::Object::BitVector->new(
+        capacity => $ks_searcher->doc_max + 1 );
+    my $collector = KinoSearch::Search::HitCollector::BitCollector->new(
+        bit_vector => $bit_vec, );
+
+    $ks_searcher->collect(
+        query     => $query_parser->parse("$query"),
+        collector => $collector
+    );
+
+    # find the facets
+    my %facets;
+    my $doc_id = 0;
+    my $count  = 0;
+    while (1) {
+        $doc_id = $bit_vec->next_set_bit( $doc_id + 1 );
+        last if $doc_id == -1;
+        last if $sample_size and ++$count > $sample_size;
+        my $doc = $ks_searcher->fetch_doc( doc_id => $doc_id );
+        for my $name (@facet_names) {
+
+            # unique-ify
+            my %val = map { $_ => $_ }
+                split( m/\003/, $doc->{$name} );
+            for my $value ( keys %val ) {
+                $facets{$name}->{$value}++;
+            }
+        }
+    }
+
+    # turn the struct inside out a bit, esp for XML
+    my %facet_struct;
+    for my $f ( keys %facets ) {
+        for my $n ( keys %{ $facets{$f} } ) {
+            push @{ $facet_struct{$f} },
+                { term => $n, count => $facets{$f}->{$n} };
+        }
+    }
+    return \%facet_struct;
 }
 
 1;
@@ -29,6 +83,10 @@ Search::OpenSearch::Engine::KSx - KinoSearch server with OpenSearch results
 =head2 init_searcher
 
 Returns a SWISH::Prog::KSx::Searcher object.
+
+=head2 build_facets( I<query>, I<results> )
+
+Returns hash ref of facets from I<results>. See Search::OpenSearch::Engine.
 
 =head1 AUTHOR
 
