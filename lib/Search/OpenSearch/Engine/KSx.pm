0ef982ed3330d9ca585292b36dd854315f9d7a7c
Peter Karman <karpet@peknet.com>
2010-10-29 03:52:54 +0000

    release 0.06

--- lib/Search/OpenSearch/Engine/KSx.pm
+++ lib/Search/OpenSearch/Engine/KSx.pm
@@ -8,7 +8,7 @@ use KinoSearch::Object::BitVector;
 use KinoSearch::Search::HitCollector::BitCollector;
 use Data::Dump qw( dump );
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub init_searcher {
     my $self     = shift;
