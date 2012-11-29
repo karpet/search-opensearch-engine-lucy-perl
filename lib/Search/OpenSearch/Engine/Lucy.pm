4335c87d2744942547834d913a6ad8d35db9982d
Peter Karman <pkarman@mpr.org>
2012-11-29 14:35:41 -0600

    fix doc

--- lib/Search/OpenSearch/Engine/Lucy.pm
+++ lib/Search/OpenSearch/Engine/Lucy.pm
@@ -357,7 +357,7 @@ Search::OpenSearch::Engine::Lucy - Lucy server with OpenSearch results
  );
  my $response = $engine->search(
     q           => 'quick brown fox',   # query
-    s           => 'rank desc',         # sort order
+    s           => 'score desc',        # sort order
     o           => 0,                   # offset
     p           => 25,                  # page size
     h           => 1,                   # highlight query terms in results
