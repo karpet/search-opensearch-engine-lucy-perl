cfb8aa7b1939420af62d6356ce25faaf2cedda9f
Peter Karman <pkarman@mpr.org>
2013-02-12 16:16:01 -0600

    update docs

--- lib/Search/OpenSearch/Engine/Lucy.pm
+++ lib/Search/OpenSearch/Engine/Lucy.pm
@@ -485,7 +485,7 @@ Search::OpenSearch::Engine::Lucy - Lucy server with OpenSearch results
     ),
     cache_ttl       => 3600,
     do_not_hilite   => [qw( color )],
-    snipper_config  => { as_sentences => 1 },        # see Search::Tools::Snipper
+    snipper_config  => { as_sentences => 1, strip_markup => 1, }, # see Search::Tools::Snipper
     hiliter_config  => { class => 'h', tag => 'b' }, # see Search::Tools::HiLiter
     parser_config   => {},                           # see Search::Query::Parser
     
